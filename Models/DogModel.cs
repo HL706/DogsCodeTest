9282785dbaf3dc2c89c3d0281eb5eddf58b35ff6
Michael Gormley <michaelgormley48@gmail.com>
2018-10-02 20:55:52 +0100

    Added a generic model that can be used for data binding a very basic 'dog' model for test purposes

---
+++ Models/DogModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Dogs.Models
+{
+    public class DogModel : Model
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+        public DogModel()
+        {
+        }
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Dogs.Models
+{
+    public class Model : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetProperty<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
+        {
+            property = value;
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
